6dfe2b7e030cd38d3d57cf166a800f4b0ba8e353
hendisantika <hendisantika@yahoo.co.id>
2019-02-10 07:54:46 +0700

    #12 - Add CustomUser

--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,16 @@
 # Register your models here.
+# users/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
